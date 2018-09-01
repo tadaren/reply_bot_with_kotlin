d8c0a42892afb106eaf1d3b9536ec586f275261a
tadaren <ptymaato@yahoo.co.jp>
2018-09-01 10:02:37 +0900

    restore database classes

--- src/main/kotlin/tadaren/Mapping.kt
+++ src/main/kotlin/tadaren/Mapping.kt
@@ -7,18 +7,14 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "reply_map")
-class Mapping{
+data class Mapping(
 
     @Id
     @Column(name = "key", nullable = false)
-    var key: String = ""
+    var key: String = "",
 
     @Column(name = "value", nullable = false)
     var value: String = ""
 
-    constructor(key: String, value: String){
-        this.key = key
-        this.value = value
-    }
 
-}
+)
