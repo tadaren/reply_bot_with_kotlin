06efbba49aed2e48d6cf444c6d3fe549b0989519
tadaren <ptymaato@yahoo.co.jp>
2018-09-06 11:58:26 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# reply_bot_with_kotlin
+
+## 概要
+登録してある文字列がlineで話された時に対応した文字列を返答するLINE_BOTです．
+
+kotlin+SpringBootで書かれています．
--- src/main/kotlin/tadaren/WebHookController.kt
+++ src/main/kotlin/tadaren/WebHookController.kt
@@ -7,16 +7,8 @@ import com.linecorp.bot.model.message.Message
 import com.linecorp.bot.model.message.TextMessage
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-import org.hibernate.criterion.Example
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Controller
-import java.sql.SQLException
-import javax.sql.DataSource
 
 
 @Component
@@ -24,75 +16,8 @@ import javax.sql.DataSource
 class WebHookController{
 
     @Autowired
-//    lateinit var mappingService: MappingService
     lateinit var mappingRepository: MappingRepository
 
-//    @Value("\${spring.datasource.url:}")
-//    private val dbUrl: String? = null
-//
-//    @Autowired
-//    lateinit var dataSource: DataSource
-//
-//    @Bean
-//    @Throws(SQLException::class)
-//    fun dataSource(): DataSource{
-//        return if(dbUrl?.isEmpty() != false){
-//            HikariDataSource()
-//        }else{
-//            val config = HikariConfig()
-//            config.jdbcUrl = dbUrl
-//            HikariDataSource(config)
-//        }
-//    }
-//
-//    fun upsertMapping(key: String, value: String): Boolean{
-//        dataSource.connection.use {
-//            return try {
-//                val statement = it.createStatement()
-//                statement.executeUpdate("CREATE TABLE IF NOT EXISTS reply_map(key TEXT NOT NULL UNIQUE, value TEXT NOT NULL)")
-//                val preStatement = it.prepareStatement("INSERT INTO reply_map VALUES (?, ?) ON CONFLICT ON CONSTRAINT reply_map_key_key DO UPDATE SET value=?")
-//                preStatement.setString(1, key)
-//                preStatement.setString(2, value)
-//                preStatement.setString(3, value)
-//                preStatement.execute()
-//                true
-//            }catch(e: SQLException) {
-//                println(e.message)
-//                false
-//            }
-//        }
-//    }
-//
-//    fun selectFromMapping(key: String): String?{
-//        dataSource.connection.use {
-//            return try {
-//                val preStatement = it.prepareStatement("SELECT value FROM reply_map WHERE key=? ")
-//                preStatement.setString(1, key)
-//                val s = preStatement.executeQuery()
-//                println("Result: $s")
-//                if(s.next())
-//                    s.getString(1)
-//                else
-//                    null
-//            }catch(e: SQLException){
-//                println("SQLException: ${e.message}")
-//                null
-//            }
-//        }
-//    }
-//
-//    fun deleteMapping(key: String): Boolean{
-//        dataSource.connection.use{
-//            return try{
-//                val preStatement = it.prepareStatement("DELETE FROM reply_map WHERE key=?")
-//                preStatement.setString(1, key)
-//                preStatement.execute()
-//            }catch(e: SQLException){
-//                println(e)
-//                false
-//            }
-//        }
-//    }
 
     @EventMapping
     @Throws(Exception::class)
@@ -102,12 +27,12 @@ class WebHookController{
 
         val splitText = text.split(Regex("(?:　(?:＝?＝|=[=>]?|->)　| (?:＝?＝|=[=>]?|->)|＝?＝|=[=>]?|->)"))
 
-        if(Regex("""https?://[\w/:%#\\${'$'}&\?\(\)~\.=\+\-]+""").containsMatchIn(splitText[0])){
+        if(Regex("""https?://[\w/:%#\\${'$'}&?()~.=+\-]+""").containsMatchIn(splitText[0])){
             return null
         }
 
         return when(splitText.size){
-            1 -> TextMessage(mappingRepository.findByKey(splitText[0])?.value)
+            1 -> mappingRepository.findByKey(splitText[0])?.value?.let { TextMessage(it) }
             2 -> {
                 mappingRepository.save(Mapping(splitText[0], splitText[1]))
                 TextMessage("success")
